1bdd41706b98cfc85b700a89012a84e884ed8b40
Karl Sixten Bjarnø <karlsixten@gmail.com>
2024-02-16 14:05:17 +0100

    Added each of the GET and POST endpoints. Added basic attractions.html page for viewing all attractions

--- src/main/java/org/example/turistguidedel2/controller/TouristController.java
+++ src/main/java/org/example/turistguidedel2/controller/TouristController.java
@@ -1,8 +1,14 @@
 package org.example.turistguidedel2.controller;
 
+import org.example.turistguidedel2.model.TouristAttraction;
 import org.example.turistguidedel2.service.TouristService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("attractions")
@@ -11,5 +17,42 @@ public class TouristController {
 
     public TouristController(TouristService touristService) {
         this.touristService = touristService;
+    }
+
+    @GetMapping("")
+    public String getAllAttractions(Model model) {
+        List<TouristAttraction> attractionList = touristService.getAttractionList();
+        model.addAttribute("attractionsList", attractionList);
+        return "attractions";
+    }
+
+    @GetMapping("{name}/tags")
+    public String getAttractionTags() {
+        return null;
+    }
+
+    @GetMapping("add")
+    public String addAttraction() {
+        return null;
+    }
+
+    @PostMapping("save")
+    public String saveAttraction() {
+        return null;
+    }
+
+    @GetMapping("{name}/edit")
+    public String editAttraction() {
+        return null;
+    }
+
+    @PostMapping("update")
+    public String updateAttraction() {
+        return null;
+    }
+
+    @GetMapping("{name}/delete")
+    public String deleteAttraction() {
+        return null;
     }
 }
